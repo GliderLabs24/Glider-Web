0b0fe988cb5a17b3703da00e7f822266d147e1e2
Goodlucklabs <goodlucklabs@gmail.com>
2025-11-15 12:37:37 +0530

    Fix import paths in App.tsx to use relative paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import { Switch, Route, useLocation, useRoute } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { AIChatButton } from "@/components/AIChatButton";
-import { CookieBanner } from "@/components/CookieBanner";
-import Home from "@/pages/Home";
-import AppPage from "@/pages/App";
-import Cookies from "@/pages/Cookies";
-import AdminPage from "@/pages/AdminPage";
-import NotFound from "@/pages/not-found";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { AIChatButton } from "./components/AIChatButton";
+import { CookieBanner } from "./components/CookieBanner";
+import Home from "./pages/Home";
+import AppPage from "./pages/App";
+import Cookies from "./pages/Cookies";
+import AdminPage from "./pages/AdminPage";
+import NotFound from "./pages/not-found";
 import { useEffect, useState } from "react";
 
 // Simple password protection for admin routes
