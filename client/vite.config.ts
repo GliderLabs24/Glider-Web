b68d433ba12a52a12648887cc093ae6ea6683b65
Goodlucklabs <goodlucklabs@gmail.com>
2025-11-16 15:07:08 +0530

    fix: update path alias configuration in Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: '@/src', replacement: path.resolve(__dirname, 'src') + '/' },
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
       { find: '@assets', replacement: path.resolve(__dirname, '..', 'attached_assets') }
     ]
   },
